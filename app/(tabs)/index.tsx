9ad36ed25280061c0a5dd93ef786dbe853204a9a
Alden Derf <aldenderfc.fabro99@gmail.com>
2025-03-12 21:12:51 +0800

    Implement the primary button design

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
 import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
 const PlaceHolderImage = require("@/assets/images/background-image.jpg");
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <ImageViewer imgSource={PlaceHolderImage} />
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -19,5 +24,10 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
+  },
+
+  footerContainer: {
+    flex: 1 / 2,
+    alignItems: "center",
   },
 });
