549c965cbc0d9513c34da596e68ef2099c225aec
Alden Derf <aldenderfc.fabro99@gmail.com>
2025-03-12 20:08:13 +0800

    Update index.tsx
    - Used the ImageViewer component in the tabl index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,23 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Image } from "expo-image";
-const PlaceHolderImage = require('@/assets/images/background-image.jpg');
+import { View, StyleSheet } from "react-native";
+import ImageViewer from "@/components/ImageViewer";
+const PlaceHolderImage = require("@/assets/images/background-image.jpg");
 
 export default function Index() {
- 
   return (
     <View style={styles.container}>
-      <Image source={PlaceHolderImage} style={styles.image} />
+      <ImageViewer imgSource={PlaceHolderImage} />
     </View>
   );
 }
 
- const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     justifyContent: "center",
-     backgroundColor: "#25292e",
-     alignItems: "center",
-   },
-   imageContainer: {
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
-   },
-   image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-   }
- });
+    justifyContent: "center",
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+});
