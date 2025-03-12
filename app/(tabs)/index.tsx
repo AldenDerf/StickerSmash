3aa1346b8a426074ee7994522672a38e2cb3e8b9
Alden Derf <aldenderfc.fabro99@gmail.com>
2025-03-12 20:04:05 +0800

    Update index.tsx
    - added an image placeholder

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+const PlaceHolderImage = require('@/assets/images/background-image.jpg');
 
 export default function Index() {
  
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello StickerSmash!</Text>
+      <Image source={PlaceHolderImage} style={styles.image} />
     </View>
   );
 }
@@ -16,12 +18,12 @@ export default function Index() {
      backgroundColor: "#25292e",
      alignItems: "center",
    },
-   text: {
-     color: "#fff",
+   imageContainer: {
+    flex: 1,
    },
-   button: {
-     fontSize: 20,
-     textDecorationLine: "underline",
-     color: "#fff",
-   },
+   image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+   }
  });
