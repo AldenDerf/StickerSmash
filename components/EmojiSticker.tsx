36782ea70dc84ea020f240cf15c2814c591d9fb8
Alden Derf <aldenderfc.fabro99@gmail.com>
2025-03-14 13:49:42 +0800

    Update EmojiSticker.tsx

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,18 +1,45 @@
-import { View } from 'react-native';
-import { type ImageSource} from 'expo-image';
-import Animated from 'react-native-reanimated';
+import { View } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import { type ImageSource } from "expo-image";
 
 type Props = {
-    imageSize: number;
-    stickerSource: ImageSource;
+  imageSize: number;
+  stickerSource: ImageSource;
 };
 
-export default function EmojiSticker({imageSize, stickerSource} : Props) {
-    return (
-        <View style={{ top: -350}}>
-            <Animated.Image 
-                source={stickerSource} 
-                style={{width: imageSize, height: imageSize}} />
-        </View>
-    );
+export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+  const scaleImage = useSharedValue(imageSize);
+
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2;
+      } else {
+        scaleImage.value = Math.round(scaleImage.value / 2);
+      }
+    });
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      heigth: withSpring(scaleImage.value),
+    };
+  });
+  return (
+    <View style={{ top: -350 }}>
+      <GestureDetector gesture={doubleTap}>
+        <Animated.Image
+          source={stickerSource}
+          resizeMode="contain"
+          style={[imageStyle, { width: imageSize, height: imageSize }]}
+        />
+      </GestureDetector>
+    </View>
+  );
 }
